3f0aa5c3cd963fde524188aef125c35364ccbddd
Haifishbro209 <165416936+Haifishbro209@users.noreply.github.com>
2025-03-03 18:45:23 +0100

    Update roman.py
    Works

--- roman.py
+++ roman.py
@@ -1,71 +1,4 @@
-def romanToInt(s):
-    I = 1
-    V = 5
-    X = 10
-    L = 50
-    C = 100
-    D = 500
-    M = 1000
-    num = 0
-    for l in range(len(s)):
-        match s[l]:
-            case "I":
-                num += I
-            case "V":
-                if l != 0:
-                    if s[l-1] == "I":
-                        num += (V-I)
-                    else:
-                       num += V
-                else:
-                    num += V
-            case "X":
-                if l != 0:
-                    if s[l-1] == "I":
-                        num += (X-I)
-                    else:
-                       num += X
-                else:
-                    num += X            
-            case "L":
-                if l != 0:
-                    if s[l-1] == "X":
-                        num += (L-X)
-                    else:
-                       num += L
-                else:
-                    num += L 
-            case "C":
-                if l != 0:
-                    if s[l-1] == "X":
-                        num += (C-X)
-                    else:
-                       num += C
-                else:
-                    num += C 
-            case "D":
-                if l != 0:
-                    if s[l-1] == "C":
-                        num += (D-C)
-                    else:
-                       num += D
-                else:
-                    num += D 
-            case "M":
-                if l != 0:
-                    if s[l-1] == "C":
-                        num += (M-C)
-                    else:
-                       num += M
-                else:
-                    num += M 
-        return num
-print(romanToInt("IV"))
-
-
-
-
-class Solution2:
+class Solution:
     def romanToInt(self, s: str) -> int:
         dictonary = {
             "I":1,
@@ -80,10 +13,7 @@ class Solution2:
         for l in range(len(s)):
             num += dictonary[s[l]]
             if l != 0:
-                if s[l-1] == "I":
-                    num += (V-I)
-                else:
-                    num += V
-            else:
-                num += V
+                if dictonary[s[l-1]] < dictonary[s[l]] :
+                    num-= 2 * dictonary[s[l-1]]
+                
         return num
