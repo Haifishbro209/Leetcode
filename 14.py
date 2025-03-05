2da7580bb7df3f1adb60afa42acce92cd1ee2c48
Haifishbro209 <165416936+Haifishbro209@users.noreply.github.com>
2025-03-05 21:02:48 +0100

    Update 14.py

--- 14.py
+++ 14.py
@@ -1,9 +1,17 @@
-prefix = None
-arr = []
-strs = ["flower","flow","flight"]
-for s in range(len(strs)):
-    string = strs[s]
-    for l in range(len(strs[s])):
-                
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        prefix = ""
+        arr = []
+        strs = ["flower","flow","flight"]
 
-
+        for l in range(len(min(strs,key = len))):
+            objs = []
+            for string in strs:
+                objs.append(string[l])
+            for objec in range(len(objs)):
+                if objec != 0:
+                    if objs[objec] != objs[objec -1]:
+                        return prefix
+            if l == len(min(strs,key = len))-1:
+                prefix = prefix + objs[0] 
+        return prefix
